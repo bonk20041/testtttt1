85fa067dcfc0870acf359e40c677d9affee61e18
bonk20041 <139465266+bonk20041@users.noreply.github.com>
2023-07-14 11:26:32 +0300

    1.1

--- REDME.MD.py
+++ REDME.MD.py
@@ -1,2 +1,2 @@
-<hi> Мой сайт </hi>
-<imp src = "https://ae04.alicdn.com/kf/H7970fc8aeeb74502a908e0a40471682cs.jpg">
+<h1> Мой сайт </h1>
+<img src = "https://ae04.alicdn.com/kf/H7970fc8aeeb74502a908e0a40471682cs.jpg">
